bb5c27f4266e0a2ed79fe21f2c8f90ed8c76b949
JustinHodge <51083905+JustinHodge@users.noreply.github.com>
2022-06-08 19:42:48 -0400

    next step - parse xml

--- src/components/FolderInput.tsx
+++ src/components/FolderInput.tsx
@@ -1,6 +1,5 @@
 const FolderInput = (props) => {
     const changeUploadedFiles = (event, uploadedFileType) => {
-        console.log(uploadedFileType);
         props.setUploadedFiles({
             ...props.uploadedFiles,
             [uploadedFileType]: event.target.files[0],
@@ -9,30 +8,38 @@ const FolderInput = (props) => {
 
     return (
         <>
-            <label> BIOMES </label>
-            <input
-                className='folder-input'
-                type='file'
-                onChange={(event) => changeUploadedFiles(event, 'biomes')}
-                accept={'image/png'}
-                multiple
-            />
-            <label> PREFABS </label>
-            <input
-                className='folder-input'
-                type='file'
-                onChange={(event) => changeUploadedFiles(event, 'prefabs')}
-                accept={'.xml'}
-                multiple
-            />
-            <label> SPAWN POINTS </label>
-            <input
-                className='folder-input'
-                type='file'
-                onChange={(event) => changeUploadedFiles(event, 'spawnpoints')}
-                accept={'.xml'}
-                multiple
-            />
+            <div>
+                <label> BIOMES </label>
+                <input
+                    className='folder-input'
+                    type='file'
+                    onChange={(event) => changeUploadedFiles(event, 'biomes')}
+                    accept={'image/png'}
+                    multiple
+                />
+            </div>
+            <div>
+                <label> PREFABS </label>
+                <input
+                    className='folder-input'
+                    type='file'
+                    onChange={(event) => changeUploadedFiles(event, 'prefabs')}
+                    accept={'.xml'}
+                    multiple
+                />
+            </div>
+            <div>
+                <label> SPAWN POINTS </label>
+                <input
+                    className='folder-input'
+                    type='file'
+                    onChange={(event) =>
+                        changeUploadedFiles(event, 'spawnpoints')
+                    }
+                    accept={'.xml'}
+                    multiple
+                />
+            </div>
         </>
     );
 };
--- src/components/MapRender.tsx
+++ src/components/MapRender.tsx
@@ -1,8 +1,21 @@
 const MapRender = (props) => {
-    console.log(props.uploadedFiles.biomes);
+    const parser = new DOMParser();
+    const reader = new FileReader();
+    let prefabXML = '';
+    let prefabs;
+    reader.onload = (event) => {
+        if (typeof reader.result === 'string') {
+            prefabXML = reader.result;
+            // console.log(prefabXML);
+            prefabs = parser.parseFromString(prefabXML, 'text/xml');
+            console.log(prefabs);
+        }
+    };
+    // reader.readAsText(props.uploadedFiles.prefabs);
     return (
         <div className='map-render'>
             <img
+                style={{ maxWidth: '85vw', maxHeight: '88vh' }}
                 src={
                     props.uploadedFiles?.biomes
                         ? URL.createObjectURL(props.uploadedFiles.biomes)
