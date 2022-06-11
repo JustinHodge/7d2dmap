eb579baf176f22b7277642fcbed624c0844fd2ba
JustinHodge <51083905+JustinHodge@users.noreply.github.com>
2022-06-10 20:37:03 -0400

    xml parsed

--- src/components/MapRender.tsx
+++ src/components/MapRender.tsx
@@ -1,17 +1,53 @@
+const createPosition = (positionString) => {
+    const positionObject = { rawPosition: positionString };
+    const positionParts = positionString.split(',');
+    const rawX = positionParts[0];
+    const rawZ = positionParts[1];
+    const rawY = positionParts[2];
+
+    positionObject.xPosition = { numericalValue: rawX };
+    positionObject.yPosition = { numericalValue: rawY };
+    positionObject.zPosition = { numericalValue: rawZ };
+
+    return positionObject;
+};
+
 const MapRender = (props) => {
-    const parser = new DOMParser();
     const reader = new FileReader();
-    let prefabXML = '';
-    let prefabs;
+    const domParser = new DOMParser();
+    let prefabs = [];
     reader.onload = (event) => {
         if (typeof reader.result === 'string') {
-            prefabXML = reader.result;
-            // console.log(prefabXML);
-            prefabs = parser.parseFromString(prefabXML, 'text/xml');
-            console.log(prefabs);
+            const prefabXMLString = reader.result;
+            if (prefabXMLString) {
+                const prefabXML = domParser.parseFromString(
+                    prefabXMLString,
+                    'text/xml'
+                );
+                const decorations =
+                    prefabXML.getElementsByTagName('decoration');
+                for (const decoration of decorations) {
+                    prefabs.push({
+                        type: decoration.getAttribute('type'),
+                        name: decoration.getAttribute('name'),
+                        position: createPosition(
+                            decoration.getAttribute('position')
+                        ),
+                        rotation: decoration.getAttribute('rotation'),
+                    });
+                }
+
+                console.log(prefabs);
+
+                prefabs.forEach((element) => {});
+            }
         }
     };
-    // reader.readAsText(props.uploadedFiles.prefabs);
+
+    if (props.uploadedFiles.prefabs) {
+        reader.readAsText(props.uploadedFiles.prefabs);
+    }
+
     return (
         <div className='map-render'>
             <img
