a1268c8e2a5ffccc76480f4e39122bf9c6a22d69
JustinHodge <51083905+JustinHodge@users.noreply.github.com>
2022-08-14 16:45:39 -0400

    replace favicon and logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,12 @@
+import logo from '../logo.svg';
 const HeaderBar = () => {
-    return <div className='header-bar'>7 Days to Die Map Builder</div>;
+    return (
+        <div className='header-bar'>
+            <img className='header-logo' src={logo}></img>
+            <h1>7 Days to Die Map Builder</h1>
+            <img className='header-logo inverted-svg' src={logo}></img>
+        </div>
+    );
 };
 
 export default HeaderBar;
